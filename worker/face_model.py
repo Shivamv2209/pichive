14b467ffcc8d7facbb825b52222e3bda5bed53d0
shivam verma <shivamv2131@gmail.com>
2026-06-17 03:56:23 +0530

    model check

--- worker/face_model.py
+++ worker/face_model.py
@@ -1,8 +1,10 @@
 from insightface.app import FaceAnalysis
 import os
 import contextlib
+import sys
 
 with open(os.devnull,"w") as f:
     with contextlib.redirect_stdout(f):
+        print("Loading model...", file=sys.stderr)
         app = FaceAnalysis(name='buffalo_l')
         app.prepare(ctx_id=-1)
