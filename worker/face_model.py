ec42a9d19d5a027fc2f62d0ff83318d7866435e5
shivam verma <shivamv2131@gmail.com>
2026-06-17 05:01:26 +0530

    face model errors

--- worker/face_model.py
+++ worker/face_model.py
@@ -1,22 +1,24 @@
 from insightface.app import FaceAnalysis
-import os
-import contextlib
 import sys
 
-with open(os.devnull, "w") as f:
-    with contextlib.redirect_stdout(f):
-        print("Loading model...", file=sys.stderr)
-        sys.stderr.flush()
+print("Loading model...", file=sys.stderr)
+sys.stderr.flush()
 
-        app = FaceAnalysis(
-            name="buffalo_l",
-            root="/opt/render/project/src/models"
-        )
+print("Before FaceAnalysis()", file=sys.stderr)
+sys.stderr.flush()
 
-        print("Before prepare()", file=sys.stderr)
-        sys.stderr.flush()
+app = FaceAnalysis(
+    name="buffalo_l",
+    root="/opt/render/project/src/models"
+)
 
-        app.prepare(ctx_id=-1)
+print("After FaceAnalysis()", file=sys.stderr)
+sys.stderr.flush()
 
-        print("After prepare()", file=sys.stderr)
-        sys.stderr.flush()
+print("Before prepare()", file=sys.stderr)
+sys.stderr.flush()
+
+app.prepare(ctx_id=-1)
+
+print("After prepare()", file=sys.stderr)
+sys.stderr.flush()
