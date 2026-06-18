20274e659677a76bd207d5d0f8524d7f72bbef9f
shivam verma <shivamv2131@gmail.com>
2026-06-19 01:20:44 +0530

    added custom domain

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(express.urlencoded({extended:true}))
 app.use(cors({
     origin:[
         "https://pichive-front.vercel.app",
+        "https://www.pichive.in/",
     ]
 }))
 app.use("/api/events",events_routes);
