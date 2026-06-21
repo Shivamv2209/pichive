fc0e6495b242e9d59de4daa256b90ee2d225fc8b
shivam verma <shivamv2131@gmail.com>
2026-06-21 19:40:30 +0530

    recursive folder tree complete

--- utils/testDrive.js
+++ utils/testDrive.js
@@ -8,13 +8,44 @@ const id = getId(
   "https://drive.google.com/drive/folders/1XehHlfNSdP9IEZZ6zjaY2_JOAjybpqsc",
 );
 
-const files = await lisFiles(id);
-// console.log(files)
 
-for (const img of files) {
-  const stream = await getfileStream(img.id);
+// SINGLE FOLDER UPLAOD TO S3 DONE 
+// const files = await lisFiles(id);
+// // console.log(files)
 
-  await uploadDriveImage(stream, `drive-test/${img.id}.jpg`, img.mimeType);
+// for (const img of files) {
+//   const stream = await getfileStream(img.id);
+
+//   await uploadDriveImage(stream, `drive-test/${img.id}.jpg`, img.mimeType);
+// }
+
+// console.log("upload done");
+
+
+// NESTED FOLDER TO S3 UPLOADS:
+
+
+const getAllImages = async (FolderId)=>{
+
+    let images=[]
+
+    const items = await lisFiles(FolderId);
+
+    for (const item of items){
+        if(item.mimeType.startsWith("image/")){
+            images.push(item)
+        }else if (item.mimeType === "application/vnd.google-apps.folder"){
+            const childImages = await getAllImages(item.id)
+            images.push(...childImages)
+        }else{
+            continue;
+        }
+    }
+
+    return images;
 }
 
-console.log("upload done");
+const images = await getAllImages(id);
+
+console.log(images.length)
+
